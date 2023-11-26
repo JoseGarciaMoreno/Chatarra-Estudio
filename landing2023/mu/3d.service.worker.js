b3a49e81646efa223097777940818ddaaa4801ca
JoseGarciaMoreno <64783493+JoseGarciaMoreno@users.noreply.github.com>
2023-11-26 17:25:29 +0100

    una hora y cuarto probando audio

--- landing2023/mu/3d.service.worker.js
+++ landing2023/mu/3d.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1701013131|1204500542";
+const CACHE_VERSION = "1701015783|577416534";
 const CACHE_PREFIX = "Marduk-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "3d.offline.html";
